f7ffc6bb1d0a95157e275f8797b69a06289fc13c
anilkumar thatha. venkatachalapathy <anilkumar.tv@eximr-im-085.local>
2016-06-03 14:09:28 +0530

    Code changes for single digit

--- palindrome/AppDelegate.m
+++ palindrome/AppDelegate.m
@@ -51,98 +51,102 @@
 - (NSInteger) palindrome: (NSUInteger )number
 {
     NSInteger palindrome = 00;
-    if (number > 0)
+    NSString *numberAsString = [NSString stringWithFormat:@"%ld", number];
+    NSLog(@"String = %@", numberAsString);
+    
+    NSUInteger characterCount = [numberAsString length];
+    NSLog(@"String Count %ld", characterCount);
+    
+    NSString *finalString = @"";
+    
+    if (characterCount % 2)
     {
-        NSString *numberAsString = [NSString stringWithFormat:@"%ld", number];
-        NSLog(@"String = %@", numberAsString);
+        // odd
+        NSLog(@"ODD");
         
-        NSUInteger characterCount = [numberAsString length];
-        NSLog(@"String Count %ld", characterCount);
+        NSUInteger i = characterCount/2;
+        // NSLog(@"centre Index - %ld",i+1);
         
-        NSString *finalString = @"";
+        NSString * middleString = [numberAsString substringWithRange:NSMakeRange(i, 1)];
+        NSLog(@"middleString - %@", middleString);
         
-        if (characterCount % 2)
+        NSString * leftString = [numberAsString substringWithRange:NSMakeRange(0, i)];
+        NSLog(@"leftString - %@", leftString);
+        
+        NSString * rightString = [numberAsString substringWithRange:NSMakeRange(i+1, i)];
+        NSLog(@"rightString - %@", rightString);
+        
+        NSString *leftMirrorString = [self reverseString:leftString];
+        NSLog(@"leftMirrorString - %@", leftMirrorString);
+        NSInteger leftNumber = [leftString integerValue];
+        
+        if ([leftMirrorString integerValue] <= [rightString integerValue])
         {
-            // odd
-            NSLog(@"ODD");
-            
-            NSUInteger i = characterCount/2;
-            // NSLog(@"centre Index - %ld",i+1);
-            
-            NSString * middleString = [numberAsString substringWithRange:NSMakeRange(i, 1)];
-            NSLog(@"middleString - %@", middleString);
-            
-            NSString * leftString = [numberAsString substringWithRange:NSMakeRange(0, i)];
-            NSLog(@"leftString - %@", leftString);
-            
-            NSString * rightString = [numberAsString substringWithRange:NSMakeRange(i+1, i)];
-            NSLog(@"rightString - %@", rightString);
-            
-            NSString *leftMirrorString = [self reverseString:leftString];
-            NSLog(@"leftMirrorString - %@", leftMirrorString);
-            NSInteger leftNumber = [leftString integerValue];
-            
-            if ([leftMirrorString integerValue] <= [rightString integerValue])
+            if ([middleString integerValue] == 9)
             {
-                if ([middleString integerValue] == 9)
+                //Add 1 to left number
+                NSUInteger leftNumberLengthBefore = [leftString length];
+                leftNumber = [leftString integerValue] + 1;
+                middleString = [NSString stringWithFormat:@"0"];
+                NSUInteger leftNumberLengthAfter = [[NSString stringWithFormat:@"%ld",leftNumber] length];
+                if (leftNumberLengthBefore != leftNumberLengthAfter)
                 {
-                    //Add 1 to left number
-                    NSUInteger leftNumberLengthBefore = [leftString length];
-                    leftNumber = [leftString integerValue] + 1;
-                    middleString = [NSString stringWithFormat:@"0"];
-                    NSUInteger leftNumberLengthAfter = [[NSString stringWithFormat:@"%ld",leftNumber] length];
-                    if (leftNumberLengthBefore != leftNumberLengthAfter)
-                    {
-                        middleString = [NSString stringWithFormat:@""];
-                    }
-                }
-                else
-                {
-                    middleString = [NSString stringWithFormat:@"%ld",[middleString integerValue]+1];
+                    middleString = [NSString stringWithFormat:@""];
                 }
             }
             else
             {
-                //Do Nothing
+                middleString = [NSString stringWithFormat:@"%ld",[middleString integerValue]+1];
             }
-            NSString *leftStr = [NSString stringWithFormat:@"%ld", leftNumber];
-            NSString * rightStr = [self reverseString:[NSString stringWithFormat:@"%ld",leftNumber]];
-            finalString = [NSString stringWithFormat:@"%@%@%@",leftStr, middleString, rightStr];
-            NSLog(@"%@",finalString);
-            
         }
         else
         {
-            // even
-            NSLog(@"EVEN");
-            
-            NSUInteger i = characterCount/2;
-            NSLog(@"centre Index - %ld",i);
-            NSString * leftString = [numberAsString substringWithRange:NSMakeRange(0, i)];
-            NSLog(@"leftString - %@", leftString);
-            
-            NSString * rightString = [numberAsString substringWithRange:NSMakeRange(i, i)];
-            NSLog(@"rightString - %@", rightString);
-            
-            NSString *leftMirrorString = [self reverseString:leftString];
-            NSLog(@"leftMirrorString - %@", leftMirrorString);
-            NSInteger leftNumber = [leftString integerValue];
-            if ([leftMirrorString integerValue] <= [rightString integerValue])
-            {
-                //Add 1 to left number
-                leftNumber = [leftString integerValue] + 1;
-            }
-            else
-            {
-                //Do Nothing
-            }
-            NSString *leftStr = [NSString stringWithFormat:@"%ld", leftNumber];
+            //Do Nothing
+        }
+        NSString *leftStr = [NSString stringWithFormat:@"%ld", leftNumber];
+        if (leftNumber == 0)
+        {
+            finalString = [NSString stringWithFormat:@"%@", middleString];
+        }
+        else
+        {
             NSString * rightStr = [self reverseString:[NSString stringWithFormat:@"%ld",leftNumber]];
-            finalString = [NSString stringWithFormat:@"%@%@",leftStr, rightStr];
+            finalString = [NSString stringWithFormat:@"%@%@%@",leftStr, middleString, rightStr];
             NSLog(@"%@",finalString);
         }
-        palindrome = [finalString integerValue];
+        
     }
+    else
+    {
+        // even
+        NSLog(@"EVEN");
+        
+        NSUInteger i = characterCount/2;
+        NSLog(@"centre Index - %ld",i);
+        NSString * leftString = [numberAsString substringWithRange:NSMakeRange(0, i)];
+        NSLog(@"leftString - %@", leftString);
+        
+        NSString * rightString = [numberAsString substringWithRange:NSMakeRange(i, i)];
+        NSLog(@"rightString - %@", rightString);
+        
+        NSString *leftMirrorString = [self reverseString:leftString];
+        NSLog(@"leftMirrorString - %@", leftMirrorString);
+        NSInteger leftNumber = [leftString integerValue];
+        if ([leftMirrorString integerValue] <= [rightString integerValue])
+        {
+            //Add 1 to left number
+            leftNumber = [leftString integerValue] + 1;
+        }
+        else
+        {
+            //Do Nothing
+        }
+        NSString *leftStr = [NSString stringWithFormat:@"%ld", leftNumber];
+        NSString * rightStr = [self reverseString:[NSString stringWithFormat:@"%ld",leftNumber]];
+        finalString = [NSString stringWithFormat:@"%@%@",leftStr, rightStr];
+        NSLog(@"%@",finalString);
+    }
+    palindrome = [finalString integerValue];
     return palindrome;
 }
 
